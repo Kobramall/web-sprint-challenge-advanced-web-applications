981b77883aa1a6711385061be90f56e62efbaa5f
guest hugh <kbramall11@gmail.com>
2022-09-29 12:36:28 -0600

    I add the test for the spinner

--- .history/frontend/components/Spinner.test_20220929123235.js
+++ .history/frontend/components/Spinner.test_20220929123235.js
@@ -1,6 +1,5 @@
 import Spinner from './Spinner'
 import { render } from "@testing-library/react"
-import React from 'react'
 // Import the Spinner component into this file and test
 // that it renders what it should for the different props it can take.
 test('renders without errors', () => {
